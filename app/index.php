3ccb7f7a874662a321770cabdfa92fdd6126608c
xxNpCxx <xxNpCxx@gmail.com>
2020-04-20 18:46:50 +0300

    fix mongodb in docker

--- app/index.php
+++ app/index.php
@@ -1,6 +1,8 @@
 <?php
 
 use BBot\Collectors\WebSocketCollector;
+use MongoDB\Client;
+use MongoDB\Driver\Exception\ConnectionTimeoutException;
 
 ini_set("default_socket_timeout", 1);
 
@@ -9,5 +11,32 @@ require __DIR__ . '/vendor/autoload.php';
 //    $collector = new testzeromq();
 
 
-$collector = new WebSocketCollector('bookTicker', 'eth', 'btc');
-$collector->run();
+//$collector = new WebSocketCollector('bookTicker', 'eth', 'btc');
+//$collector->run();
+
+
+
+//TODO
+//exchange
+//pair
+//type
+$client = (new Client(
+    'mongodb://root:ggg12345678ggg@mongodb:27017'
+));
+
+try {
+    $dbs = $client->listDatabases();
+} catch (ConnectionTimeoutException $e) {
+    var_dump($e->getMessage());
+    // PHP cannot find a MongoDB server using the MongoDB connection string specified
+    // do something here
+}
+
+var_dump($dbs);
+
+//$result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
+
+/*
+$collector = new WebSocketCollector('bookTicker', 'btc', 'usdt');
+$collector->run();*/
+
---
+++ mongodb.Dockerfile
@@ -0,0 +1,16 @@
+FROM alpine:3.11
+
+RUN echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/main' >> /etc/apk/repositories
+RUN echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/community' >> /etc/apk/repositories
+RUN apk update
+
+RUN apk add openssl-dev
+RUN apk add mongodb --no-cache
+
+VOLUME /data/db
+EXPOSE 27017 28017
+
+COPY run.sh /root
+RUN chmod a+x /root/run.sh
+ENTRYPOINT [ "/root/run.sh" ]
+CMD [ "mongod", "--bind_ip", "0.0.0.0" ]
