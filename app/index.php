2c90c95b27a261be8c036942baf4498cdb044e99
Stepan Peredriy <stepan.peredriy@icandeliver.ru>
2020-04-22 18:51:40 +0300

    Создать в базе local коллекцию binance
    Usage:
    php index.php type=bookTicker mainSymbol=btc quoteSymbol=usdt

--- app/index.php
+++ app/index.php
@@ -7,11 +7,14 @@ ini_set("default_socket_timeout", 1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-//TODO
-//exchange
-//pair
-//type
+echo "Usage php index.php type=bookTicker mainSymbol=btc quoteSymbol=usdt\n";
 
-$collector = new WebSocketCollector('bookTicker', 'btc', 'usdt');
+foreach ($argv as $arg) {
+    [$key, $value] = explode('=',$arg);
+    $$key = $value;
+}
+
+
+$collector = new WebSocketCollector($type, $mainSymbol, $quoteSymbol);
 $collector->run();
 
