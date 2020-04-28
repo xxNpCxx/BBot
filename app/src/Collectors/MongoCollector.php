05e14359921b9a4287a4f1802af4e9bf45242071
xxNpCxx <xxNpCxx@gmail.com>
2020-04-28 03:31:13 +0300

    Изменил способ хранения данных.
    Теперь каждая коллекция называется по имени биржи, типу хранимой информации и символьной паре.

--- app/src/Collectors/MongoCollector.php
+++ app/src/Collectors/MongoCollector.php
@@ -7,13 +7,15 @@ namespace BBot\Collectors;
 use MongoDB\Client;
 use MongoDB\Collection;
 use function json_decode;
+use function microtime;
 use function printf;
+use function sprintf;
+use function strtolower;
 
 class MongoCollector extends TCPSocketSubscriber
 {
     const DEFAULT_STORAGE_DSN = 'mongodb://mongodb:27017';
     const DEFAULT_MONGO_DATABASE = 'local';
-    const DEFAULT_MONGO_COLLECTION = 'binance-price';
 
     /**
      * @var Client
@@ -21,37 +23,65 @@ class MongoCollector extends TCPSocketSubscriber
     private $storageClient;
     private $dbName;
     private $collectionName;
+
     /**
      * @var Collection
      */
     private $collection;
 
-    private $collectingIdentityName;
+    /**
+     * @var array
+     */
+    private $collectedItemInfo;
 
     protected function onDataReceived(string $data)
     {
-        $jsonData = json_decode($data);
-        $this->collection->insertOne([$this->collectingIdentityName => $jsonData]);
+        $preparedDocument = $this->prepareDocumentToInsert($data);
+        $this->collection->insertOne($preparedDocument);
         printf('.');
     }
 
-    public function __construct(string $dataProviderEndpoint, string $collectingIdentityName = null)
+    public function __construct(string $dataProviderEndpoint, array $collectedItemInfo = [])
     {
         parent::__construct($dataProviderEndpoint);
-
         $this->dbName = self::DEFAULT_MONGO_DATABASE;
-        $this->collectionName = self::DEFAULT_MONGO_COLLECTION;
-
-        $this->collectingIdentityName = $collectingIdentityName;
+        $this->collectedItemInfo = $collectedItemInfo;
+        $this->setCollectionName();
         $this->connectToStorage();
         $this->collection = $this->storageClient->selectCollection($this->dbName, $this->collectionName);
 
+    }
+
+    private function prepareDocumentToInsert(string $data)
+    {
+        $jsonData = json_decode($data);
+        $document = [
+            't' => microtime(true),
+            'data' => $jsonData
+        ];
+        return $document;
     }
 
     private function connectToStorage(string $dsn = self::DEFAULT_STORAGE_DSN, array $options = []): bool
     {
         $this->storageClient = new Client($dsn, $options);
         return true;
+    }
+
+    private function setCollectionName()
+    {
+        $mainSymbol = $this->collectedItemInfo['mainSymbol'] ?? 'undefinedMainSymbol';
+        $quoteSymbol = $this->collectedItemInfo['quoteSymbol'] ?? 'undefinedQuoteSymbol';
+        $exchangeName = $this->collectedItemInfo['exchange'] ?? 'undefinedExchange';
+        $typeName = $this->collectedItemInfo['type'] ?? 'undefinedType';
+
+        $this->collectionName = sprintf(
+            '%s_%s_%s%s',
+            $exchangeName,
+            $typeName,
+            $mainSymbol,
+            $quoteSymbol
+        );
     }
 
 }
