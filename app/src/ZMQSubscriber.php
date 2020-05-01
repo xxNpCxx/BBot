04f43e9dcb3d6722c584871c1e461f0daf7dcf11
xxNpCxx <xxNpCxx@gmail.com>
2020-05-01 16:25:48 +0300

    Теперь ZMQSubscriber может подписываться на несколько маршрутов.

--- app/src/ZMQSubscriber.php
+++ app/src/ZMQSubscriber.php
@@ -4,52 +4,50 @@
 namespace BBot;
 
 
-use BBot\TCPSocketRoutes;
 use ZMQ;
 use ZMQContext;
 use ZMQSocket;
 use function printf;
-use function printf as printfAlias;
-use function sprintf;
-use function usort;
 
 abstract class ZMQSubscriber
 {
     private $context;
     private $subscriber;
-    private $route;
+    private $routes;
     private $endpoint;
 
 
-    protected function onDataReceived(string $data)
+    protected function onDataReceived(string $route, string $data)
     {
         echo('onDataReceived'.PHP_EOL);
     }
 
-    public function __construct( string $endpointstring, $route = TCPSocketRoutes::ROUTE_ALL)
+    public function __construct( string $endpointstring, array $routes = [TCPSocketRoutes::ROUTE_ALL])
     {
         $this->endpoint = $endpointstring;
-        $this->route = $route;
+        $this->routes = $routes;
         $this->context = new ZMQContext();
         $this->subscriber = new ZMQSocket($this->context, ZMQ::SOCKET_SUB);
-        $this->subscriber->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, $this->route);
+        foreach ($routes as $route) {
+            $this->subscriber->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, $route);
+        }
         $this->connect();
     }
 
     private function connect()
     {
         $this->subscriber->connect($this->endpoint);
-        printfAlias('Connection successfull to [%s]%s', $this->endpoint, PHP_EOL);
+        printf('Connection successfull to [%s]%s', $this->endpoint, PHP_EOL);
 
     }
 
     public function listen()
     {
-        printfAlias('Start listen [%s] route %s', $this->route, PHP_EOL);
+        printf('Start listen [%s] route %s', $this->routes, PHP_EOL);
         while (true) {
             $route = $this->subscriber->recv();
             $contents = $this->subscriber->recv();
-            $this->onDataReceived($contents);
+            $this->onDataReceived($route, $contents);
         }
     }
 }
