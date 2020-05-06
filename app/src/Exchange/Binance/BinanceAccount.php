a4b783e07aacd615e635375d864f2437590fa8f7
xxNpCxx <xxNpCxx@gmail.com>
2020-05-06 23:58:24 +0300

    Update BinanceAccount.php

--- app/src/Exchange/Binance/BinanceAccount.php
+++ app/src/Exchange/Binance/BinanceAccount.php
@@ -5,41 +5,65 @@ namespace BBot\Exchange\Binance;
 
 
 use BBot\Exchange\Account;
+use ccxt\binance;
+use Symfony\Component\Yaml\Yaml;
 
-class BinanceAccount implements Account
+class BinanceAccount extends binance implements Account
 {
 
-    private $apiKey;
-    private $secretKey;
-    private $email;
+    private $_apiKey;
+    private $_secretKey;
+    private $_email;
+
+    public function __construct($email)
+    {
+        $this->setEmail($email);
+        $account = Yaml::parseFile(__DIR__.'/../../../accounts.yaml')['binance']['emails'][$email];
+
+        $this->setApiKey($account['apiKey']);
+        $this->setSecretKey($account['secretKey']);
+
+        $ccxtOptions = [
+            'apiKey' => $this->_apiKey,
+            'secret' => $this->_secretKey,
+            'verbose' => false
+        ];
+
+        parent::__construct($ccxtOptions);
+    }
 
     public function setApiKey(string $apiKey)
     {
-        $this->apiKey = $apiKey;
+        $this->_apiKey = $apiKey;
     }
 
     public function setSecretKey(string $secretKey)
     {
-        $this->secretKey = $secretKey;
+        $this->_secretKey = $secretKey;
     }
 
     public function setEmail(string $email)
     {
-        $this->email = $email;
+        $this->_email = $email;
     }
 
-    public function getEmail()
+    public function getName()
     {
-        return $this->email;
+        return $this->_email;
     }
 
     public function getSecretKey()
     {
-        return $this->secretKey;
+        return $this->_secretKey;
     }
 
     public function getApiKey()
     {
-        return $this->apiKey;
+        return $this->_apiKey;
+    }
+
+    public function getBalance()
+    {
+        return $this->fetchBalance();
     }
 }
