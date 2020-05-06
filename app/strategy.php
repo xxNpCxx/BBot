37fcb3e558226a43b066ababf82c6e3cdb3c5536
xxNpCxx <xxNpCxx@gmail.com>
2020-05-06 23:58:57 +0300

    Update strategy.php

--- app/strategy.php
+++ app/strategy.php
@@ -1,21 +1,24 @@
 <?php
 
-use BBot\Strategy\TestStrategy;
+use BBot\Exchange\AbstractClient;
+use BBot\Exchange\Client;
 
 require __DIR__ . '/vendor/autoload.php';
+
+printf('Usage: php strategy.php exchange=binance strategyName=Test mainSymbol=xrp quoteSymbol=btc accountEmail=xxNpCxx@gmail.com %s', PHP_EOL);
 
 foreach ($argv as $arg) {
     [$key, $value] = explode('=',$arg);
     $$key = $value;
 }
-$options = [
-    ''
-];
-$strategyName = ucfirst($strategyName);
-$strategyClass = sprintf('BBot\Strategy\%sStrategy',$strategyName);
 
-//TODO: Заменить на интерфейс стратегии
-/**
- * @var TestStrategy
- */
-$strategy = new $strategyClass($options);
+$strategyName = ucfirst($strategyName);
+$strategyClass = sprintf('\BBot\Strategy\%sStrategy',$strategyName);
+
+/** @var Client|AbstractClient $strategy */
+/** @var string $exchangeName */
+/** @var string $accountEmail */
+/** @var string $mainSymbol */
+/** @var string $quoteSymbol */
+
+$strategy = new $strategyClass($exchange, $accountEmail, $mainSymbol, $quoteSymbol);
